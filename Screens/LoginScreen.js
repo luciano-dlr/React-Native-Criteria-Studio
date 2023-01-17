9eeef360374b68c0a80a4cd71dd926de89734033
Luciano de la Rubia <delarubialuciano@gmail.com>
2023-01-17 15:52:51 -0300

    limpiando codigo

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,30 +1,14 @@
 import { useNavigation } from "@react-navigation/native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useState } from "react";
 import styles from "../Styles/Styles";
 import { firebaseConfig } from "../firebase-config.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import React from "react";
-import { Text, View, Image, TouchableOpacity, Alert, TextInput, Button } from 'react-native';
+import { View, Image, Alert, Button } from 'react-native';
 import { Input } from '@rneui/themed';
 
-import { registerScreen } from "./RegisterScreen.js";
-import { HomeScreen } from "./HomeScreen.js";
-
-import { signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
-
-
-
-
-
-export const LoginScreen = ({ states }) => {
-
-    // const [password, email] = states;
-
-    // states = [email, password]
-    // console.log(states)
+export const LoginScreen = () => {
 
     const navigation = useNavigation();
 
@@ -32,15 +16,10 @@ export const LoginScreen = ({ states }) => {
     const [email, setEmail] = useState('');
 
     const app = initializeApp(firebaseConfig);
-
     const auth = getAuth(app);
 
-    const provider = new GoogleAuthProvider();
-
-
-
-
     const ingresarCuenta = () => {
+
         signInWithEmailAndPassword(auth, email, password)
 
             .then(() => {
@@ -59,45 +38,27 @@ export const LoginScreen = ({ states }) => {
             })
     };
 
-    const googleBoton = () => {
-
-        signInWithRedirect(auth, provider)
-            .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                // The signed-in user info.
-                const user = result.user;
-                // ...
-            }).catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.customData.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                // ...
-            });
-    }
-
     return (
         <View style={styles.container}>
 
             <Image source={require('../assets/logoCriteria.jpg')} style={styles.imagen} />
 
             <Input type='email' id='email' style={styles.imputs} placeholder='Ingresar Email' onChangeText={(text) => setEmail(text)} />
+
             <Input secureTextEntry={true} id='password' style={styles.imputs} placeholder='Ingresar Contraseña' onChangeText={(text) => setPassword(text)} />
 
             <View >
+
                 <Button title="Login" onPress={ingresarCuenta} />
+
             </View>
-            <View >
-                <Button title="Google" onPress={googleBoton} />
-            </View>
+
             <View>
-                <Button title="registerScreen" onPress={() => { navigation.navigate("registerScreen") }} ></Button>
+
+                <Button title="registerScreen" onPress={() => { navigation.navigate("registerScreen") }} />
+
             </View>
+
         </View>
     );
 
--- Screens/RegisterScreen.js
+++ Screens/RegisterScreen.js
@@ -1,145 +1,79 @@
-// import React from 'react';
-// import { useState } from 'react';
-// import { Text, View, Image, TouchableOpacity, Button, Input } from 'react-native';
-// import { useNavigation } from "@react-navigation/native";
-// import styles from "../Styles/Styles";
-// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-// import { initializeApp } from "firebase/app";
-// import { firebaseConfig } from "../firebase-config.js";
-
-
-
-
-
-
-// export const RegisterScreen = () => {
-
-//     // states = [email, password]
-//     // console.log(states)
-//     const [password, setPassword] = useState('');
-//     const [email, setEmail] = useState('');
-
-//     const app = initializeApp(firebaseConfig);
-
-//     const auth = getAuth(app);
-
-//     const crearCuenta = () => {
-//         createUserWithEmailAndPassword(auth, email, password)
-//             .then(() => {
-//                 console.log("cuenta creada");
-//                 const user = auth.currentUser;
-//                 console.log(user)
-//                 Alert.alert("bienvenido " + user.email)
-
-//             })
-//             .catch(() => {
-//                 console.log("error")
-//             })
-//     }
-
-//     return (
-//         <View>
-//             <View>
-//                 <Input type='email' id='email' style={styles.imputs} placeholder='Ingresar Email' onChangeText={(text) => setEmail(text)} />
-//                 <Input secureTextEntry={true} id='password' style={styles.imputs} placeholder='Ingresar Contraseña' onChangeText={(text) => setPassword(text)} />
-//             </View>
-//             <View >
-//                 <Button title="Registrar" />
-//             </View>
-//         </View>
-//     )
-// };
-
-
 
 import { useNavigation } from "@react-navigation/native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useState } from "react";
 import styles from "../Styles/Styles";
 import { firebaseConfig } from "../firebase-config.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, Cre } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import React from "react";
-import { Text, View, Image, TouchableOpacity, Alert, TextInput, Button } from 'react-native';
+import { Text, View, Alert, Button } from 'react-native';
 import { Input } from '@rneui/themed';
 
-import { registerScreen } from "./RegisterScreen.js";
-import { HomeScreen } from "./HomeScreen.js";
+
+
 
 
 
 
 export const RegisterScreen = () => {
 
-    // const [password, email] = states;
-
-    // states = [email, password]
-    // console.log(states)
-
     const navigation = useNavigation();
 
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
 
     const app = initializeApp(firebaseConfig);
-
     const auth = getAuth(app);
 
-
-
-
-    // const ingresarCuenta = () => {
-    //     signInWithEmailAndPassword(auth, email, password)
-
-    //         .then(() => {
-
-    //             const user = auth.currentUser;
-    //             console.log({ user });
-    //             console.log("logeado papa!");
-    //             navigation.navigate("home")
-    //             Alert.alert("Ya estas logueado" + " " + user.email)
-
-    //         })
-    //         .catch(() => {
-
-    //             console.log("error en login ")
-    //             Alert.alert("Usuario y contraseña no registrados")
-    //         })
-    // };
-
     const crearCuenta = () => {
-        createUserWithEmailAndPassword(auth, email, password, name)
+        createUserWithEmailAndPassword(auth, email, password)
             .then(() => {
                 console.log("cuenta creada");
                 const user = auth.currentUser;
                 console.log(user)
-                Alert.alert("bienvenido " + user.email)
+                Alert.alert("bienvenido " + user.email);
 
             })
             .catch(() => {
-                console.log("error")
+                // console.log("error")
             })
     }
+    const handleSubmit = () => {
+        if (password !== confirmPassword) {
+            setError(' Las contraseñas no coinciden ');
+        } else {
+            setError('');
+            // Perform other actions
+            crearCuenta();
+        }
+    };
 
     return (
+
         <View style={styles.container}>
 
-            {/* <Image source={require('../assets/logoCriteria.jpg')} style={styles.imagen} /> */}
-
             <Input type='email' id='email' style={styles.imputs} placeholder='Ingresar Email' onChangeText={(text) => setEmail(text)} />
-            <Input secureTextEntry={true} id='password' style={styles.imputs} placeholder='Ingresar Contraseña' onChangeText={(text) => setPassword(text)} />
-            <Input id='name' style={styles.imputs} placeholder='Ingresar Nombre' onChangeText={(text) => setName(text)} />
 
+            <Input secureTextEntry={true} id='password' style={styles.imputs} placeholder='Ingresar Contraseña' onChangeText={(text) => setPassword(text)} />
+
+            {error !== '' && <Text style={{ color: 'red' }}>{error}</Text>}
+            <Input placeholder="Confirmar Contraseña" value={confirmPassword} onChangeText={(text) => setConfirmPassword(text)} secureTextEntry={true} />
 
             <View >
-                <Button title="Registrarse" onPress={crearCuenta} />
+                <Text style={{ color: 'white', fontSize: 12, }}>Contraseña minimo 6 caracteres</Text>
+
+                <Button title="Registrarse" onPress={handleSubmit} />
+
             </View>
             <View>
+
                 <Button title="Regresar al login " onPress={() => { navigation.navigate("login") }} ></Button>
             </View>
+
         </View>
+
     );
 
 };
