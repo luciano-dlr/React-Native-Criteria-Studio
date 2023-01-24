b1340239947947e2a9f3d55c5969a5f01352f328
Luciano de la Rubia <delarubialuciano@gmail.com>
2023-01-24 15:42:20 -0300

    limpiando codigo

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
+import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { registerRootComponent } from 'expo';
-
-import React from "react";
 
 // Pantallas 
 import { HomeScreen } from "./Screens/HomeScreen.js";
@@ -20,14 +18,10 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator Container initialRouteName="login">
 
-        <Stack.Screen name="login" component={LoginScreen} options={{
-          headerShown: false,
-        }} />
+        <Stack.Screen name="login" component={LoginScreen} options={{ headerShown: false, }} />
         <Stack.Screen name="registerScreen" component={RegisterScreen} />
         <Stack.Screen name="home" component={HomeScreen} />
         <Stack.Screen name="cotizacion" component={CotizacionScreen} />
-
-
 
       </Stack.Navigator>
     </NavigationContainer >
@@ -35,16 +29,3 @@ const App = () => {
 };
 
 export default App;
-
-//exp:\192.168.0.13:19000
-
-// Los componentes que dejan renderizar web son @renui
-
-// carpeta de rutas, si un suario se loguea, necesitas rutas publicas
-
-
-//                app con usuarios
-
-//rutas publicas son aquellas que el usuario puede acceder sin estar logueado. registre y login
-
-//rutas privadas son aquellas rutas que necesitan una auth para que el usuario pueda acceder a ellas, ej home prifle cotizacion etc
